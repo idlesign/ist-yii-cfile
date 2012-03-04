795791eda131530c63e3c1fed17212a4c6f379d3
idle sign <idlesign@yandex.ru>
2012-03-04 15:30:38 +0700

    Stab for unit tests is added.

---
+++ tests/tests.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once '../CFile.php';
+
+
+define('TESTS_DIR', dirname(__FILE__));
+
+
+class CFileTests extends PHPUnit_Framework_TestCase {
+
+    public function testTestsDir() {
+
+    }
+
+}
