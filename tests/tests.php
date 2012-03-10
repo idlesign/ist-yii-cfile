e8b304802a125824d995257798bc9933053877f9
idle sign <idlesign@yandex.ru>
2012-03-10 13:26:27 +0700

    Added basic CFileHelper directory tests.

--- tests/tests.php
+++ tests/tests.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../CFile.php';
+require_once '../CFileHelper.php';
 
 
 define('TESTS_DIR', dirname(__FILE__));
@@ -9,7 +9,17 @@ define('TESTS_DIR', dirname(__FILE__));
 class CFileTests extends PHPUnit_Framework_TestCase {
 
     public function testTestsDir() {
-
+        $cf = CFileHelper::get(TESTS_DIR);
+        $this->assertTrue($cf->getExists());
+        $this->assertEquals('tests', $cf->getBasename());
+        $this->assertEquals('tests', $cf->getFilename());
+        $this->assertEquals('', $cf->getExtension());
+        $this->assertTrue($cf->getIsDir());
+        $this->assertFalse($cf->getIsEmpty());
+        $this->assertFalse($cf->getIsFile());
+        $this->assertFalse($cf->getIsUploaded());
+        $this->assertTrue($cf->getReadable());
+        $this->assertTrue($cf->getWriteable());
     }
 
 }
